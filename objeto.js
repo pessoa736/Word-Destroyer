ccb48ae2a235f713e958f1d4bf41b027cf2a6a56
Davi dos Santos Passos <69681778+pessoa736@users.noreply.github.com>
2024-10-07 15:55:46 +0000

    update objetos.js

--- objeto.js
+++ objeto.js
@@ -1,85 +1,108 @@
-class bloco {
-  constructor(XTamanhoBloco, YTamanhoBloco, Xbloco, Ybloco, r, g, b) {
-    this.XTamanhoBloco = XTamanhoBloco;
-    this.YTamanhoBloco = YTamanhoBloco;
-    this.Xbloco = Xbloco;
-    this.Ybloco = Ybloco;
-    this.r = r;
-    this.g = g;
-    this.b = b;
+class vec2{
+  constructor(x,y){
+    this.x = x
+    this.y = y 
   }
-  tamanho() {
-    fill(this.r, this.g, this.b)
-    rect(
-      windowWidth * this.Xbloco - (windowWidth * this.XTamanhoBloco) * 0.5,
-      windowHeight * this.Ybloco - (windowHeight * this.YTamanhoBloco) * 0.5,
-      windowWidth * this.XTamanhoBloco,
-      windowHeight * this.YTamanhoBloco,
-      5
-    )
+  add(b){
+    let a = this
+    return vec2(a.x + b.x, a.y + b.y)
+  }
+  sub(b){
+    let a = this
+    return vec2(a.x - b.x, a.y - b.y)
+  }
+  mult(b){
+    let a = this
+    return vec2(a.x * b.x, a.y * b.y)
+  }
+}
+
+class color{
+  constructor(r, g, b){
+    this.r = r
+    this.g = g
+    this.b = b 
+  }
+}
+
+class bloco {
+  constructor(size, position, color) {
+    this.size = size
+    this.position = position
+    this.color = color
+
+    let windowsize = vec2(windowWidth, windowHeight)
+    this.view = {
+      pos: windowsize.mult(
+        this.position.sub(
+          windowsize.mult( this.size ).mult(
+            vec2(0.5, 0.5)
+          )
+        )
+      ),
+      size: windowsize.mult(this.position)
+    }
+    
+  }
+  draw() {
+    fill(this.color.r, this.color.g, this.color.b)
+    rect(this.view.pos.x, this.view.pos.y, this.view.size.x, this.view.size.y, 5);
   }
   click(){
-    let x = windowWidth * this.Xbloco - (windowWidth * this.XTamanhoBloco) * 0.5
-    let y = windowHeight * this.Ybloco - (windowHeight * this.YTamanhoBloco) * 0.5
-    let w =  windowWidth * this.XTamanhoBloco
-    let h = windowHeight * this.YTamanhoBloco
+
+    let x = this.view.pos.x
+    let y = this.view.pos.y
+    let w = this.view.size.x
+    let h = this.view.size.y
+
     if (mouseX >= x && mouseX <= x + w && mouseY >= y && mouseY <= y + h){
       return true
     }
   }
 }
 
-class imagem {
-  constructor(Caminhoimagem, Xtamanhoimagem, Ytamanhoimagem, Ximg, Yimg) {
+class imagem extends bloco{
+  constructor(Caminhoimagem, size, position) {
+    super(size, position, color(0,0,0))
+
     this.Caminhoimagem = Caminhoimagem;
-    this.Xtamanhoimagem = Xtamanhoimagem;
-    this.Ytamanhoimagem = Ytamanhoimagem;
-    this.Ximg = Ximg;
-    this.Yimg = Yimg;
   }
-  tamanho_estatico() {
+  draw() {
     image(
       this.Caminhoimagem,
-      windowWidth * this.Ximg - (windowWidth * this.Xtamanhoimagem) * 0.5,
-      windowHeight * this.Yimg - (windowHeight * this.Ytamanhoimagem) * 0.5,
-      windowWidth * this.Xtamanhoimagem,
-      windowHeight * this.Ytamanhoimagem
+      this.view.pos.x,
+      this.view.pos.y,
+      this.view.size.x,
+      this.view.size.y
     );
-  }
-  tamanho_animado() {
-    image(this.Caminhoimagem,
-      windowWidth * this.Ximg,
-      windowHeight * this.Yimg,
-      windowWidth * this.Xtamanhoimagem,
-      windowHeight * this.Ytamanhoimagem
-    )
   }
 }
 
 class PalavrasObj extends bloco{
-  constructor(XTamanhoBloco, YTamanhoBloco, Xbloco, Ybloco, r, g, b,) {
-    super(XTamanhoBloco, YTamanhoBloco, Xbloco, Ybloco, r, g, b);
+  constructor(size, position, color) {
+    super(size, position, color);
     this.palavras = EscolherPalavras();
   }
-  obj() {
-    super.tamanho();
+  draw() {
+    super.draw();
 
     textWrap(CHAR);
     textAlign(CENTER, BASELINE);
     textSize(windowWidth * 0.03);
+
     if(this.palavras == Digitacao){
       fill(46, 255, 65);
     }else{
       fill(255, 50, 0)
     }
-    text(this.palavras, windowWidth * this.Xbloco, windowHeight * (this.Ybloco + 0.0155));
+    text(this.palavras, windowWidth * this.position.x, windowHeight * (this.position.y + 0.0155));
 
     if (verificacao == true) {
-      this.Ybloco += 0.00025
+      this.position.y += 0.00025
     }
   }
   check_palavra(digitacao){
-    if (digitacao == this.palavras && this.Ybloco<0.75){
+    if (digitacao == this.palavras && this.position.y<0.75){
       return true
     }else{
       return false
